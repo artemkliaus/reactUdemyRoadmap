4ea456a10efca2997c517c21c5141f998cba9906
a.kliaus <a.kliaus@a.kliaus>
2018-09-20 17:39:33 +0300

    add css library

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -10,8 +10,8 @@ class Card extends Component {
         let position = this.props.data.positionShort;
 
         return (
-            <div className='card'>
-                <div className='card__img'><img src='https://placehold.it/100x200/'/></div>
+            <div className='card bg-dark-green ba b--light-gray br2 grow'>
+                <div className='card__img'><img src={`https://robohash.org/${name}?size=200x200`}/></div>
                 <div>{name}</div>
                 <div>{surname}</div>
                 <div className='card__pos'>{position}</div>
