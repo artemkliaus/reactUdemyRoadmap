fa0d939a9f9b2829f09904b67650db7d7422d36f
a.kliaus <a.kliaus@a.kliaus>
2018-09-21 16:32:41 +0300

    add fetch method

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,15 +5,15 @@ class Card extends Component {
 
     render(props) {
 
-        let name = this.props.data.firstName;
-        let surname = this.props.data.lastName;
+        let name = this.props.data.name;
+        let email = this.props.data.email;
         let position = this.props.data.positionShort;
 
         return (
             <div className='card bg-dark-green ba b--light-gray br2 grow'>
                 <div className='card__img'><img src={`https://robohash.org/${name}?size=200x200`}/></div>
                 <div>{name}</div>
-                <div>{surname}</div>
+                <div>{email}</div>
                 <div className='card__pos'>{position}</div>
             </div>
         )
--- src/components/cardWrapper/CardWrapper.js
+++ src/components/cardWrapper/CardWrapper.js
@@ -3,48 +3,23 @@ import React, { Component } from 'react';
 //Component
 import Card from '../card/Card';
 
-
-
 class CardWrapper extends Component {
 
     constructor (props) {
         super(props);
-        this.getRoster();
         this.state = {
             json: null
         }
     }
 
-    getRoster () {
-
-        let self = this;
-        const xhr = new XMLHttpRequest();
-        const method = 'Get';
-        const url = 'https://cors.io/?https://data.nba.net/prod/v1/allstar/2017/AS_roster.json';
-
-        xhr.open(method, url);
-        xhr.send();
-
-
-        xhr.onreadystatechange = function () {
-            if (this.readyState !== 4) {
-                return false;
-            } else {
-
-                let response = JSON.parse(this.responseText);
-
-                if (response) {
-                    let players = response.sportsContent.roster[0].players['1610616834'];
-
-                    self.setState({
-                        json: players
-                    });
-                    return players;
-                }
-            }
-
-        };
-
+    componentDidMount () {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(response => {
+            this.setState({
+                json: response
+            });
+        })
     }
 
     renderCards () {
@@ -62,7 +37,7 @@ class CardWrapper extends Component {
         let newJson;
 
         newJson = json.filter(function(item) {
-            if (item.firstName.toLowerCase().includes(text) || item.lastName.toLowerCase().includes(text)) return true;
+            if (item.name.toLowerCase().includes(text)) return true;
         });
 
         if (newJson.length) {
@@ -74,14 +49,13 @@ class CardWrapper extends Component {
 
     render () {
         if (this.state.json){
-
-                return (
-                    <div>
-                        {this.renderCards()}
-                    </div>
-                )} else {
-                    return <div>Loading...</div>
-                }
+            return (
+                <div>
+                    {this.renderCards()}
+                </div>
+            )} else {
+                return <div>Loading...</div>
+        }
     }
 }
 
